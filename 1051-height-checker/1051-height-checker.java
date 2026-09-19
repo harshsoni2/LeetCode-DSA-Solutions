b32bfe01d935cd7d73aa88d4d301efa9fda242e4
harshsoni2 <168360603+harshsoni2@users.noreply.github.com>
2026-09-19 20:43:58 +0530

    Time: 3 ms (86.9%), Space: 43.3 MB (74.21%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] expected = heights.clone();
+
+        for(int i = 0; i < expected.length - 1; i++){
+            boolean swapped = false;
+            for(int j = 0; j < expected.length - 1 - i; j++){
+                if(expected[j] > expected[j + 1]){
+                    int temp = expected[j];
+                    expected[j] = expected[j + 1];
+                    expected[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+        int count = 0;
+        for(int i = 0; i < heights.length; i++){
+            if(heights[i] != expected[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
